24a196b47e8ceb79b8af9bd82edfda7b95aa30d5
Tenny Ma <tenny.ma10@gmail.com>
2023-07-13 20:18:23 +0900

    change platform in gemfile.lock

---
+++ app/controllers/bookmarks_controller.rb
@@ -0,0 +1,24 @@
+class BookmarksController < ApplicationController
+  def new
+    @bookmark = Bookmark.new
+  end
+
+  def create
+    @bookmark = Bookmark.new
+  end
+
+  def destroy
+    @bookmark.destroy
+  end
+
+  private
+
+  def set_restaurant
+    @restaurant = Restaurant.find(params[:restaurant_id])
+  end
+
+  def review_params
+    params.require(:review).permit(:content)
+  end
+end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,21 @@
 Rails.application.routes.draw do
+  get 'bookmarks/new'
+  get 'bookmarks/create'
+  get 'bookmarks/destroy'
+  get 'lists/index'
+  get 'lists/show'
+  get 'lists/new'
+  get 'lists/create'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root 'lists#index'
+
+  resources :lists, only: [:index, :show, :new, :create] do
+    resources :bookmarks, only: [:new, :create]
+  end
+  resources :bookmarks, only: :destroy
 end
+
+# root to: lists#index goes straight to localhost/3000 Home URL
+# destroy separate good practice, after nesting
